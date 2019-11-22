31fd94dad0c25485ed7c5759a0e7fb0b4a8b0b5f
bborn <brandon.born@vanderbilt.edu>
2019-11-22 12:43:46 -0600

    Script for random starting position

---
+++ Individual Project/Assets/RandomStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStart : MonoBehaviour
+{
+    private float spawnDist = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = new Vector3(Random.Range(-spawnDist, spawnDist), 10, Random.Range(-spawnDist, spawnDist));
+    }
+}
